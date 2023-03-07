38470cf9e57c9073f45839a49e3c330d960eb336
Reid Miller <reid.s.miller@gmail.com>
2023-03-07 09:09:11 -0700

    test: Add tests for item class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,1 +1,13 @@
 require 'spec_helper'
+
+RSpec.describe Item do
+  describe '#Iteration 1' do
+    it 'can instantiate with name and price' do
+      item1 = Item.new({name: 'Peach', price: '$0.75'})
+      item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+      expect(item2.name).to eq('Tomato')
+      expect(item2.price).to eq(0.5)
+    end
+  end
+end
